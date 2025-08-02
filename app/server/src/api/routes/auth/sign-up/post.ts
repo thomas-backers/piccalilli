503be0350ae15cec2b01d921a800f2cbfdd640e0
thomas-backers <thomas.backers@tuta.io>
2025-08-02 10:27:45 +0200

    Added bcryptjs

--- app/server/src/api/routes/auth/sign-up/post.ts
+++ app/server/src/api/routes/auth/sign-up/post.ts
@@ -1,7 +1,15 @@
+import { ok } from "@/api/helper/status/success";
+import bcrypt from "bcryptjs";
 import type { Request, Response } from "express";
+import { SignUpForm } from "shared/validation/schemas/sign-up";
 
 const post = async (request: Request, response: Response): Promise<void> => {
-  //
+  const { username, password }: SignUpForm = request.body;
+  const passwordHash: string = await bcrypt.hash(password, 12);
+  return ok(response, "", {
+    username,
+    verified: false,
+  });
 };
 
 export default post;
